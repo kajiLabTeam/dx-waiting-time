5345a036abcdb390d5d01ea9024a87a7fdfe250f
Bayashin <k21099kk@aitech.ac.jp>
2023-11-08 18:59:25 +0900

    構文エラーの修正

--- backend/build/mysql/init/init.sql
+++ backend/build/mysql/init/init.sql
@@ -1,16 +1,16 @@
-CREATE TABLE IF NOT EXISTS Owner(
-    ID INT NOT NULL AUTO_INCREMENT;
-    URL VARCHAR(512),
-    PRIMARY KEY (ID)
-    ) ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS dx_waiting.owner(
+    id INT NOT NULL AUTO_INCREMENT,
+    url VARCHAR(512),
+    PRIMARY KEY (id)
+);
 
-CREATE TABLE IF NOT EXISTS Customer(
-    ID INT NOT NULL AUTO_INCREMENT;
-    Number INT;
-    Status VARCHAR(128);
-    Date VARCHAR(256);
-    Firebase_token VARCHAR(256);
-    Owner_ID INT;
-    PRIMARY KEY (ID);
-    foreign key (Owner_ID) references dx-waiting.Owner(ID)
-)
+CREATE TABLE IF NOT EXISTS dx_waiting.customer(
+    id INT NOT NULL AUTO_INCREMENT,
+    number INT,
+    status VARCHAR(128),
+    date VARCHAR(256),
+    firebase_token VARCHAR(256),
+    owner_id INT,
+    PRIMARY KEY (id),
+    foreign key (owner_id) references dx_waiting.owner(ID)
+);
