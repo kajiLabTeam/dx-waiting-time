4a4f664a7ffa5a65362131407ce0e9d0d9d906e2
kanakanho <kahoshibakichi@gmail.com>
2023-11-09 20:22:14 +0900

    Add: QRcodeを表示と保存機能

--- frontend/src/pages/qrPage.tsx
+++ frontend/src/pages/qrPage.tsx
@@ -1,17 +1,27 @@
 import { FC } from "react";
 import styled from "styled-components";
 import Button from "../components/layout/Button";
+import QrCode from "../components/qrcode/QrCode";
 
 const QrPageContainer = styled.div``;
 
 const QrPage: FC = () => {
-  const onDownload = () => {};
-  return (
-    <QrPageContainer>
-      <div>QR Page</div>
-      <Button message={"画像をダウンロード"} onClick={onDownload}/>
-    </QrPageContainer>
-  );
+    const onDownload = () => {
+        const canvas = document.querySelector("canvas");
+        const pngUrl = canvas?.toDataURL("image/png");
+        const downloadLink = document.createElement("a");
+        downloadLink.href = pngUrl!;
+        downloadLink.download = "qr.png";
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+    };
+    return (
+        <QrPageContainer>
+            <QrCode url={"https://www.google.com"} />
+            <Button message={"画像をダウンロード"} onClick={onDownload} />
+        </QrPageContainer>
+    );
 };
 
 export default QrPage;
