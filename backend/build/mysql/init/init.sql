8ea3dca4b62a11c64305f4229ab46406e09a9443
Bayashin <k21099kk@aitech.ac.jp>
2023-11-09 11:11:33 +0900

    updata:idのカラム名を変更

--- backend/build/mysql/init/init.sql
+++ backend/build/mysql/init/init.sql
@@ -1,16 +1,16 @@
 CREATE TABLE IF NOT EXISTS dx_waiting.owner(
-    id INT NOT NULL AUTO_INCREMENT,
+    owner_id INT NOT NULL AUTO_INCREMENT,
     url VARCHAR(512),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS dx_waiting.customer(
-    id INT NOT NULL AUTO_INCREMENT,
+    customer_id INT NOT NULL AUTO_INCREMENT,
     number INT,
     status VARCHAR(128),
     date VARCHAR(256),
     firebase_token VARCHAR(256),
     owner_id INT,
-    PRIMARY KEY (id),
-    foreign key (owner_id) references dx_waiting.owner(ID)
+    PRIMARY KEY (customer_id),
+    foreign key (owner_id) references dx_waiting.owner(owner_id)
 );
