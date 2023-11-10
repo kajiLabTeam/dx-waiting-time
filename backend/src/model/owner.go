8d4fcd4d628237955ffcf78eb991a23a7d7dc55e
Bayashin <k21099kk@aitech.ac.jp>
2023-11-10 17:40:28 +0900

    ownerIdをstringで扱えるようにした

--- backend/src/model/owner.go
+++ backend/src/model/owner.go
@@ -5,10 +5,10 @@ package model
 // 返り値：Owner, error
 // 1. ownerIdを元に、Ownerを検索
 // 2. ownerIdが存在しない場合、Ownerを作成
-func CreateOwner(ownerId int) (Owner, error) {
+func CreateOwner(ownerId, ownerName, url string) (Owner, error) {
 	o := Owner{}
 	db.Where("owner_id = ?", ownerId).First(&o)
-	if o.OwnerId == 0 {
+	if o.OwnerId == "" {
 		o.OwnerId = ownerId
 		db.Create(&o)
 	}
@@ -20,7 +20,7 @@ func CreateOwner(ownerId int) (Owner, error) {
 // 返り値：Owner, error
 // 1. OwnerIdを元に、Ownerを検索
 // 2. Ownerを削除
-func DeleteOwner(ownerId int) (Owner, error) {
+func DeleteOwner(ownerId string) (Owner, error) {
 	o := Owner{}
 	db.Where("owner_id = ?", ownerId).Find(&o)
 	db.Delete(&o)
@@ -31,7 +31,7 @@ func DeleteOwner(ownerId int) (Owner, error) {
 // 引数：OwnerId
 // 返り値：Owner, error
 // 1. OwnerIdを元に、Ownerを検索
-func GetOwner(ownerId int) (Owner, error) {
+func GetOwner(ownerId string) (Owner, error) {
 	o := Owner{}
 	db.Where("owner_id = ?", ownerId).Find(&o)
 	return o, nil
