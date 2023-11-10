42a9bfda54a2bd40d81dc15ead0425fab069801a
kanakanho <kahoshibakichi@gmail.com>
2023-11-10 18:49:41 +0900

    Refactor QR code page and message circle component

--- frontend/src/pages/qrPage.tsx
+++ frontend/src/pages/qrPage.tsx
@@ -6,35 +6,35 @@ import { QrCode } from "../utils/QrCode";
 const QrPageContainer = styled.div``;
 
 const QrCodeContainer = styled.div`
-    margin: 4rem 1rem;
+  margin: 4rem 1rem;
 `;
 
 const ButtonContainer = styled.div`
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 `;
 
 const QrPage: FC = () => {
-    const onDownload = () => {
-        const canvas = document.querySelector("canvas");
-        const pngUrl = canvas?.toDataURL("image/png");
-        const downloadLink = document.createElement("a");
-        downloadLink.href = pngUrl!;
-        downloadLink.download = "qr.png";
-        document.body.appendChild(downloadLink);
-        downloadLink.click();
-        document.body.removeChild(downloadLink);
-    };
-    return (
-        <QrPageContainer>
-            <QrCodeContainer>
-                <QrCode url={"https://www.google.com/"} />
-            </QrCodeContainer>
-            <ButtonContainer>
-                <Button message={"画像をダウンロード"} onClick={onDownload} />
-            </ButtonContainer>
-        </QrPageContainer>
-    );
+  const onDownload = () => {
+    const canvas = document.querySelector("canvas");
+    const pngUrl = canvas?.toDataURL("image/png");
+    const downloadLink = document.createElement("a");
+    downloadLink.href = pngUrl!;
+    downloadLink.download = "qr.png";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  };
+  return (
+    <QrPageContainer>
+      <QrCodeContainer>
+        <QrCode url={"https://www.google.com/"} />
+      </QrCodeContainer>
+      <ButtonContainer>
+        <Button message={"画像をダウンロード"} onClick={onDownload} />
+      </ButtonContainer>
+    </QrPageContainer>
+  );
 };
 
 export default QrPage;
--- frontend/src/utils/QrCode.tsx
+++ frontend/src/utils/QrCode.tsx
@@ -4,38 +4,38 @@ import styled from "styled-components";
 import { theme } from "./theme";
 
 type Props = {
-    url: string;
+  url: string;
 };
 
 const QrCodeContainer = styled.div`
-    text-align: center;
-    padding: 1.5rem;
-    background-color: ${theme.colors.cream};
-    border-radius: 2rem;
-    border: 0.3rem solid ${theme.colors.brown};
+  text-align: center;
+  padding: 1.5rem;
+  background-color: ${theme.colors.cream};
+  border-radius: 2rem;
+  border: 0.3rem solid ${theme.colors.brown};
 `;
 
 export const QrCode: FC<Props> = ({ url }) => {
-    const { Canvas } = useQRCode();
-    const darkColor = theme.colors.brown;
-    const lightColor = theme.colors.cream;
+  const { Canvas } = useQRCode();
+  const darkColor = theme.colors.brown;
+  const lightColor = theme.colors.cream;
 
-    return (
-        <QrCodeContainer>
-            <Canvas
-                text={url}
-                options={{
-                    type: "image/png",
-                    quality: 1,
-                    margin: 1,
-                    width: 300,
-                    color: {
-                        // RGBAで色を決める。
-                        dark: darkColor,
-                        light: lightColor,
-                    },
-                }}
-            />
-        </QrCodeContainer>
-    );
+  return (
+    <QrCodeContainer>
+      <Canvas
+        text={url}
+        options={{
+          type: "image/png",
+          quality: 1,
+          margin: 1,
+          width: 300,
+          color: {
+            // RGBAで色を決める。
+            dark: darkColor,
+            light: lightColor,
+          },
+        }}
+      />
+    </QrCodeContainer>
+  );
 };
