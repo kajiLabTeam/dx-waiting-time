2dacc80eb112911932fd617730a458d1e8b31085
kanakanho <kahoshibakichi@gmail.com>
2023-11-11 22:44:28 +0900

    fix: _documentの言語設定

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ja">
       <Head>
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@500&display=swap"
