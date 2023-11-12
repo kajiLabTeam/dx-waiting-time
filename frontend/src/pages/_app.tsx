c18e5f4d46bbce1af9f5e149cc557bc70016339f
kanakanho <kahoshibakichi@gmail.com>
2023-11-12 16:02:46 +0900

    Fix indentation in _app.tsx

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,8 +4,8 @@ import "ress";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
