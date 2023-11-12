127a5de81ab016dec003238e1bca1d187cddd08e
kanakanho <kahoshibakichi@gmail.com>
2023-11-12 22:23:53 +0900

    add: トークンの発行と、webpush通知の受信

--- frontend/src/pages/user/clientPage.tsx
+++ frontend/src/pages/user/clientPage.tsx
@@ -1,5 +1,3 @@
-import "firebase/messaging";
-import firebase from "firebase/app";
 import { getMessaging, getToken } from "firebase/messaging";
 import { FC, useEffect } from "react";
 import styled from "styled-components";
@@ -45,46 +43,35 @@ const ButtonContainer = styled.div`
   width: 100%;
 `;
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAQCD40J2m4b3i0WT9AvmzmZPpVKWaepXc",
-  authDomain: "web-push-notification-practise.firebaseapp.com",
-  projectId: "web-push-notification-practise",
-  storageBucket: "web-push-notification-practise.appspot.com",
-  messagingSenderId: "844132776020",
-  appId: "1:844132776020:web:d9cbdeaef0ebb6b72555c1",
-  measurementId: "G-B3513M6L7T",
-};
-
 const followingNumber = 3;
 const callNumber = 321;
 
+const useInitFirebase = async () => {
+  useEffect(() => {
+    const requestNotificationPermission = async () => {
+      const permission = await Notification.requestPermission();
+      if (permission !== "granted") {
+        throw new Error("Permission not granted for Notification");
+      }
+
+      const messaging = getMessaging(app);
+      const currentToken = await getToken(messaging);
+      if (currentToken) {
+        console.log("currentToken:");
+        console.log(currentToken);
+      } else {
+        console.error("No Instance ID token available. Request permission to generate one.");
+      }
+    };
+
+    requestNotificationPermission();
+  }, []);
+};
+
 const ClientPage: FC = () => {
   const onLogin = () => {};
+  useInitFirebase();
 
-  useEffect(() => {
-    const messaging = getMessaging(app);
-  
-    Notification.requestPermission().then((permission) => {
-      if (permission === "granted") {
-        // 通知を許可した場合
-        console.log("Notification permission granted.");
-        getToken(messaging).then((currentToken: string | null) => {
-          if (currentToken) {
-            // トークン取得成功
-            console.log("currentToken:");
-            console.log(currentToken);
-          } else {
-            // トークン取得失敗
-            console.log("No Instance ID token available. Request permission to generate one.");
-          }
-        });
-      } else {
-        // 通知を拒否した場合
-        console.log("Unable to get permission to notify.");
-      }
-    }, []);
-  });
   return (
     <ClientPageContainer>
       <CircleContainer>
