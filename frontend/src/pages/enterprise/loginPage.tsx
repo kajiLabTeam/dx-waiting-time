b6960792b66ede02e43c23be8463283241d773cc
happy663 <tatu2425@gmail.com>
2023-11-13 12:06:43 +0900

    Add Firebase authentication and redirect to start page upon login

--- frontend/src/pages/enterprise/loginPage.tsx
+++ frontend/src/pages/enterprise/loginPage.tsx
@@ -1,7 +1,10 @@
-import { FC } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useRouter } from "next/navigation";
+import { FC, useEffect, useState } from "react";
 import styled from "styled-components";
 import { Button } from "../../components/utils/Button";
 import { MessageCricle } from "../../components/utils/MessageCricle";
+import { googleLogin, useIsSigned } from "../../utils/auth";
 
 const LoginPageContainer = styled.div`
   text-align: center;
@@ -20,14 +23,24 @@ const Action = styled.div`
 `;
 
 const LoginPage: FC = () => {
-  const onLogin = () => {};
+  const router = useRouter();
+  const isLogin = useIsSigned();
+
+  if (isLogin === undefined) return <></>;
+  if (isLogin) router.push("/enterprise/startPage");
+
   return (
     <LoginPageContainer>
       <AppName>
         <MessageCricle message={"appName"} />
       </AppName>
       <Action>
-        <Button message={"ログイン"} onClick={onLogin} />
+        <Button
+          message={"ログイン"}
+          onClick={() => {
+            googleLogin();
+          }}
+        />
       </Action>
     </LoginPageContainer>
   );
