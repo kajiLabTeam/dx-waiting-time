16a8821c79aa532d483d49be4c6c5230465a75ec
happy663 <tatu2425@gmail.com>
2023-11-13 12:06:30 +0900

    Add RecoilRoot to _app.tsx

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
 import Layout from "../components/layout/layout";
 import "ress";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <RecoilRoot>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </RecoilRoot>
   );
 }
