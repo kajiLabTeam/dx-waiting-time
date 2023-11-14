d12d0db90b8fa506350b0dbfbb13a4274b0df53a
kanakanho <kahoshibakichi@gmail.com>
2023-11-14 18:40:14 +0900

    fix: インデックスからuserのページにリダイレクトするように変更

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,17 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useRecoilState } from "recoil";
+import { userState } from "../globalStates/firebaseUserState";
+
 export default function Home() {
+  const [key] = useRecoilState(userState);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!key) return;
+    const url = `/user/${key.uid}`;
+    router.replace(url);
+  }, [key, router]);
+
   return <div>Home</div>;
 }
