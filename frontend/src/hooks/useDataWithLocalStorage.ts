56da46933aa726bfddb88f55779dea26b2ea3030
happy663 <tatu2425@gmail.com>
2023-11-15 18:12:58 +0900

    Add filterfetcher function to useDataWithLocalStorage hook

--- frontend/src/hooks/useDataWithLocalStorage.ts
+++ frontend/src/hooks/useDataWithLocalStorage.ts
@@ -1,13 +1,25 @@
 import { useEffect } from "react";
 import useSWR from "swr";
 
-export const fetcher = async (url: string) => {
-  const response = await fetch(url);
-  return response.json();
+export const filterfetcher = async (url: string) => {
+  const noewDate = new Date();
+  console.log(localStorage.getItem("dxWaitingTime"));
+  // ローカルストレージのdateを取得
+  const localDate = JSON.parse(localStorage.getItem("dxWaitingTime") || "{}").date;
+  console.log(localDate);
+
+  // ローカルストレージと現在の日付が違う場合はAPIを叩く
+  if (localDate != null || localDate !== noewDate.toDateString()) {
+    const response = await fetch(url);
+    console.log(response);
+    return response.json();
+  }
+  // ローカルストレージと現在の日付が同じ場合はローカルストレージを返す
+  return JSON.parse(localStorage.getItem("dxWaitingTime") || "{}");
 };
 
 export const useDataWithLocalStorage = <T>(url: string) => {
-  const { data, error } = useSWR<T>(url, fetcher);
+  const { data, error } = useSWR<T>(url, filterfetcher);
 
   useEffect(() => {
     if (data) {
