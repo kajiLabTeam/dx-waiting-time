aae44b21826565818391187ef92bc48e7a5b748f
Bayashin <k21099kk@aitech.ac.jp>
2023-11-15 22:37:32 +0900

    add:cors対応

--- backend/src/router/router.go
+++ backend/src/router/router.go
@@ -1,18 +1,61 @@
 package router
 
 import (
+	"io"
+	"os"
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/kajiLabTeam/dx-waiting-time/controller"
 )
 
 func Init() {
+
+	// サーバーログの出力先を設定
+	gin.DisableConsoleColor()
+	f, _ := os.Create("../server.log")
+	gin.DefaultWriter = io.MultiWriter(f)
+
+	// ルーティングの設定
+	router := gin.Default()
+
+	// ここからCorsの設定
+	router.Use(cors.New(cors.Config{
+		// アクセスを許可したいアクセス元
+		AllowOrigins: []string{
+			"https://dx-waiting-time.vercel.app/",
+			"https://dxwaitingtime.vercel.app/",
+			"http://localhost:3000",
+		},
+		// アクセスを許可したいHTTPメソッド(以下の例だとPUTやDELETEはアクセスできません)
+		AllowMethods: []string{
+			"POST",
+			"GET",
+			"OPTIONS",
+		},
+		// 許可したいHTTPリクエストヘッダ
+		AllowHeaders: []string{
+			"Access-Control-Allow-Credentials",
+			"Access-Control-Allow-Headers",
+			"Content-Type",
+			"Content-Length",
+			"Accept-Encoding",
+			"Authorization",
+		},
+		// cookieなどの情報を必要とするかどうか
+		AllowCredentials: true,
+		// preflightリクエストの結果をキャッシュする時間
+		MaxAge: 24 * time.Hour,
+	}))
+
 	// gin.DisableConsoleColor()
 	// f, _ := os.Create("../server.log")
 	// gin.DefaultWriter = io.MultiWriter(f)
 
 	// r := gin.Default()
 	// r.Use(cors.Default())
-	r := gin.Default()
+	// r := gin.Default()
 
 	r.GET("api/:ownerId/queue/position/", controller.GetCustomer)
 
