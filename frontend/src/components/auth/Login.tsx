a1f3fafcd38bcb90ee3b85de1067dce78a600394
happy663 <tatu2425@gmail.com>
2023-11-16 00:52:44 +0900

    Remove unnecessary code and refactor login function.

--- frontend/src/components/auth/Login.tsx
+++ frontend/src/components/auth/Login.tsx
@@ -1,9 +1,6 @@
-import { User } from "firebase/auth";
 import { FC, useEffect } from "react";
 import styled from "styled-components";
-import { useUserState } from "../../globalStates/firebaseUserState";
 import { useLoginMutators } from "../../globalStates/loginState";
-import { baseURL } from "../../utils/api";
 import { googleLogin } from "../../utils/auth";
 import Layout from "../layout/layout";
 import { Button } from "../utils/Button";
@@ -26,30 +23,6 @@ const Action = styled.div`
 `;
 
 const Login: FC = () => {
-  const user = useUserState();
-  const handleLogin = async (user: User | null) => {
-    try {
-      await googleLogin();
-      console.log("login success");
-      // tokenを取得
-      const token = await user?.getIdToken();
-      // postする
-      const response = await fetch(`${baseURL}/owner/queue/create`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          " Authorization": `Bearer ${token}`,
-        },
-      });
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      console.log("post success");
-    } catch (error) {
-      console.error("Login failed: ", error);
-    }
-  };
-
   const { setLoginOpenState } = useLoginMutators();
   useEffect(() => {
     setLoginOpenState(true);
@@ -64,8 +37,8 @@ const Login: FC = () => {
         <Action>
           <Button
             message={"ログイン"}
-            onClick={() => {
-              handleLogin(user);
+            onClick={async () => {
+              await googleLogin();
             }}
           />
         </Action>
