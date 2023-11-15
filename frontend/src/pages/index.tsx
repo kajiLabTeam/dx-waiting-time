2e87d71fdadd670f18bf6a82ed08b452cc4f7b4f
kanakanho <kahoshibakichi@gmail.com>
2023-11-15 14:57:20 +0900

    fix: お店のQRコードを読み取るように誘導

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,16 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { useRecoilState } from "recoil";
+import styled from "styled-components";
 import { userState } from "../globalStates/firebaseUserState";
+import { theme } from "../utils/theme";
+
+const Text = styled.p`
+  margin: 1rem 1rem 0 1rem;
+  font-size: 2rem;
+  font-family: "Noto Sans JP", sans-serif;
+  color: ${theme.colors.brown};
+`;
 
 export default function Home() {
   const [key] = useRecoilState(userState);
@@ -13,5 +22,5 @@ export default function Home() {
     router.replace(url);
   }, [key, router]);
 
-  return <div>Home</div>;
+  return <Text>お店のQRコードを読み取って列に並ぼう！</Text>;
 }
