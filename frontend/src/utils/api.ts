3abc20c6d601c66148512cc6177555dc24532e21
happy663 <tatu2425@gmail.com>
2023-11-17 13:50:59 +0900

    Update API base URL

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,7 +1,7 @@
 import useSWR from "swr";
 
 // envファイルから取得
-export const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+export const baseURL = `${process.env.NEXT_PUBLIC_BASE_URL}/api`;
 
 export const endpoints = {
   // user
