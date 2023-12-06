69772f6c9f981110f7cee57eb5e9e44af67ef8c8
kanakanho <kahoshibakichi@gmail.com>
2023-12-06 18:52:28 +0900

    add: 業務終了ページにグラフを描画する

---
+++ frontend/src/components/gragh/Customer.ts
@@ -0,0 +1,4 @@
+export type Customer = {
+    time: number;
+    count: number;
+  };
--- frontend/src/pages/enterprise/endPage.tsx
+++ frontend/src/pages/enterprise/endPage.tsx
@@ -1,11 +1,18 @@
 import { FC } from "react";
 import styled from "styled-components";
+import { Customer } from "../../components/gragh/Customer";
+import { Graph } from "../../components/gragh/Gragh";
 import { Button } from "../../components/utils/Button";
 import { useSalesMutators, useSalesState } from "../../globalStates/salesState";
 import { theme } from "../../utils/theme";
 
+export type CustomersData = {
+  counter: number;
+  data: Customer[];
+};
+
 const EndPageContainer = styled.div`
-  margin-top: 30vh;
+  margin-top: 10vh;
 `;
 
 const ThisDateContainer = styled.p`
@@ -16,12 +23,16 @@ const ThisDateContainer = styled.p`
   color: ${theme.colors.brown};
 `;
 
+const GraphContainer = styled.div`
+  margin: 1rem;
+`;
+
 const ButtonContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   width: 100%;
-  margin: 10px;
+  margin: 5vw 0;
 `;
 
 const Text = styled.p`
@@ -31,6 +42,22 @@ const Text = styled.p`
   font-family: "Noto Sans JP", sans-serif;
   color: ${theme.colors.brown};
 `;
+
+const customers: CustomersData = {
+  counter: 0,
+  data: [
+    { time: 10, count: 20 },
+    { time: 11, count: 50 },
+    { time: 12, count: 80 },
+    { time: 13, count: 60 },
+    { time: 14, count: 10 },
+    { time: 15, count: 20 },
+    { time: 16, count: 40 },
+    { time: 17, count: 30 },
+    { time: 18, count: 60 },
+    { time: 19, count: 40 },
+  ],
+};
 
 const EndPage: FC = () => {
   const isSales = useSalesState();
@@ -50,6 +77,10 @@ const EndPage: FC = () => {
       <ThisDateContainer>
         {thisMonth}月{thisDay}日({thisWeek})
       </ThisDateContainer>
+      <ThisDateContainer>本日の来客数：{customers.counter}人</ThisDateContainer>
+      <GraphContainer>
+        <Graph customers={customers.data} />
+      </GraphContainer>
       <ButtonContainer>
         <Button message={"営業終了"} onClick={onClose} />
       </ButtonContainer>
